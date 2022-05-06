bc8b9bfc6848441d50c5e0bd60f1d8661a5f382f
Koizumi1989 <08033431635a@gmail.com>
2022-05-06 18:31:59 +0900

    sound user

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,10 +1,38 @@
 class SoundsController < ApplicationController
+
+  def new
+    @sound = Sound.new
+  end
+
   def index
+    @sounds = Sound.all
   end
 
   def show
   end
 
+  def create
+    @sound = Sound.new(sound_params)
+    @sound.user_id = current_user.id
+    if @sound.save
+      redirect_to sound_path(@sound.id)
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   def edit
   end
+
+  private
+  def sound_params
+    params.require(:sound).permit(:title, :artist, :introduction, :jenre, :rate)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  resources :users, only: [:index, :show, :edit]
-  resources :sounds, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :sounds, only: [:new, :index, :show, :create, :update, :destroy, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
